2cb7a06c1d27aa884e14486b0979446c5b0ba9b7
valderosch <phonelix96@gmail.com>
2023-01-27 00:15:12 +0200

    cleaned

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+
+
+    return(
+        <div className="header">
+            <div className="logo">
+                <h1><b>IDEA</b>flow</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
