10a2fd547f7c737f93195181900934ec9c4ace7c
valderosch <phonelix96@gmail.com>
2023-01-29 16:58:39 +0200

    bodyprep

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Postlist from "./Components/body/Postlist";
 import Header from "./Components/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Postlist/>
     </div>
   );
 }
---
+++ src/Components/body/Postlist.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Postlist = () => {
+    return(
+        <div className="postlist">
+            Response?
+        </div>
+    );
+}
+
+export default Postlist;
