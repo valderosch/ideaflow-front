ca8b595d945fae710a029fedcb4bb9eb92683573
valderosch <phonelix96@gmail.com>
2023-02-08 20:44:11 +0200

    some style
    refactored some dirs and styles

--- src/Components/body/Postlist.jsx
+++ src/Components/body/Postlist.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import "../../styles/Post.scss";
+import "../../styles/Body/post/Post.scss";
 import { useState, useEffect } from "react";
 import axios from 'axios';
-import {  Link, useParams } from "react-router-dom";
+import {  Link, useNavigate, useParams } from "react-router-dom";
+import PostOpen from "./PostOpen";
 
 const GETALL_URL = "http://localhost:8080/api/posts"; 
 
@@ -11,6 +12,7 @@ const GETALL_URL = "http://localhost:8080/api/posts";
 const Postlist = () => {
     const [posts, setPosts] = useState([]);
     const {id} = useParams()
+    const nav = useNavigate();
 
     useEffect(() => {
         loadPosts();
@@ -38,9 +40,7 @@ const Postlist = () => {
         console.log("clicked post");
         return(
             <div className="post">
-                <h4 className="post_title">{}</h4>
-                <p className="post_description">{}</p>
-                <p className="author">{}</p>
+                {nav(`/posts/${id}`)}
             </div>
         );
     }
@@ -48,15 +48,15 @@ const Postlist = () => {
     return(
         <div className="postlist">
             {posts.map((data) => (
-                 <div className="post" key={data.id} onClick={openPost}> 
-                 <h2 className="post__title">{data.title}</h2>
-                 <p>{data.decription}</p>
-                 <span>{data.author}</span>
-                 <button className="post_delete" onClick = {() => deletePost(data.id)}>{data.id} DELETE</button>
-                 <Link className="post_delete" to={`/edit/${data.id}`}> EDIT</Link>
-                 <Link className="post_open" to={`/posts/${data.id}`}> OPEN</Link>
-             </div>   
-            ))}
+                 <div className="post" key={data.id} onClick={() => openPost(data.id)}> 
+                    <h2 className="post__title">{data.title}</h2>
+                    <p>{data.decription}</p>
+                    <span>{data.author}</span>
+                    <button className="post_delete" onClick = {() => deletePost(data.id)}>x</button>
+                    <Link className="post_edit" to={`/edit/${data.id}`}>EDIT</Link>
+                    <div className="boxxx"></div>
+                </div>   
+                ))}
         </div>
     );
 }
--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -3,12 +3,22 @@ import "../../styles/Header/Header.scss";
 import SearchBar from "./Searchbar";
 import {useState} from 'react';
 import User from "./User";
+import { useNavigate } from "react-router-dom";
+
 
 //Header Component
 const Header = () => {
+    const nav = useNavigate();
+
+    const returnHomePage = () => {
+        return(
+            <div>{nav('/')}</div>
+        );
+    }
+
     return(
         <div className="header">
-            <div className="logo">
+            <div className="logo" onClick={returnHomePage}>
                 <h1 className="idea_h1"><b className="idea_b">IDEA</b>Flow</h1>
             </div>
             <SearchBar/>
