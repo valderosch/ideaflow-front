7334570f985db52cd59f9df6242637463c88ee29
valderosch <phonelix96@gmail.com>
2023-02-12 20:41:39 +0200

    hook
    added custom hook

--- src/Components/body/Feed.jsx
+++ src/Components/body/Feed.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import axios from 'axios';
 
 export const api = axios.create( {
-    GETALL_URL: 'http://localhost:8080/api/posts'
+    GETALL_URL: 'http://localhost:8080/api'
 })
 
 const Feed = async(pageParam = 1, options={}) => {
-
+    const response = await api.get(`/posts?_page=${pageParam}`, options)
+    return response.data;
 }
 
 export default Feed;
---
+++ src/hooks/usePosts.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { Feed } from './Components/body/Feed.jsx'
+
+const usePost = (pageNum = 1) => {
+    const [error, setError] = useState({});
+    const [results, setResults] = useState([]);
+    const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasNextPage, setHasNextPage] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true)
+        setIsError(false)
+        setError({})
+
+        const controller = new AbortController()
+        const {signal} = controller;
+
+        Feed(pageNum, {signal})
+            .then(data => {
+                setResults(pre => [...prev, ...data])
+                setHasNextPage(Boolean(data.lenght))
+                setIsLoading(false);
+            })
+            .catch(e => {
+                setIsLoading(false);
+                if(signal.aborted) return 
+                setIsError(true);
+                setError({message: e.message})
+            })        
+    
+        return () => controller.abort()
+    }, [pageNum])
+
+    return { isLoading, isError, error, results, hasNextPage}
+}
+
+export default usePost;
